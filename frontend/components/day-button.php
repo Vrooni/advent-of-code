e79bf704dbdb2ed6f316e56c6e60aa94a946761a
Vrooni <laura.adler@gmx.de>
2025-09-21 20:41:57 +0200

    added check if solution exists in backend

--- frontend/components/day-button.php
+++ frontend/components/day-button.php
@@ -1,5 +1,14 @@
+<?php
+$solution_exists =
+  file_exists("backend/solutions/java-solutions/year$year/Day$day.java") ||
+  file_exists("backend/solutions/java-solutions/year$year/Da" . $day . "_1.java") ||
+  file_exists("backend/solutions/java-solutions/year$year/Da" . $day . "_2.java") ||
+  file_exists("backend/solutions/php-solutions/year$year/Day$day.php");
+?>
+
+
 <form action="frontend/pages/solution.php" method="post">
-  <button class="day-button" name="day-and-year" value="<?php echo "$day:$year" ?>" type="submit">
+  <button class="day-button <?php echo $solution_exists ? "solution" : "" ?>" name="day-and-year" value="<?php echo "$day:$year" ?>" type="submit">
     <?php echo $day ?>
   </button>
 </form>
