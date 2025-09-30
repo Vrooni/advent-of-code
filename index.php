7e93ef543134e9132910bcd4ea97da0c9cf31be2
Vrooni <laura.adler@gmx.de>
2025-09-30 22:33:41 +0200

    fixed not wanted scroll to right

--- index.php
+++ index.php
@@ -1,7 +1,6 @@
 <?php
 
 // TODO all solution files has to be splitted into part 1 and 2. Good luck, have fun to me!
-// TODO add snowflakes to background
 if (isset($_GET["year"])) {
   $selected_year = $_GET["year"];
 }
